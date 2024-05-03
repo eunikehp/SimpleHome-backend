09853f0e9a3c38be032e7ecb8be9ba302f459f61
Eunike Pardede <eunikehedriani@gmail.com>
2024-05-03 13:50:30 +0200

    Basic Authentication

--- app.js
+++ app.js
@@ -33,11 +33,40 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//authentication
+function auth(req,res,next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error ('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  //parse the authorization header and validate username and password
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  // once the string is decoded, it shows the username and password separately admin:password => ['admin','password']
+  const user = auth[0];
+  const pass = auth[1];
+  if ( user === 'admin' && pass === 'password') {
+    return next(); //authorized
+  } else {
+    const err = new Error ('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status= 401;
+    return next(err)
+  }
+}
+app.use(auth);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
